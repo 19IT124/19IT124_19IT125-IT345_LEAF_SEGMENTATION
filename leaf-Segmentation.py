9bcc725e2c780065fbb9f9f0c452d2d470d469b1
19IT124 <68839514+19IT124@users.noreply.github.com>
2021-11-10 14:58:21 +0530

    Update leaf-Segmentation.py

--- leaf-Segmentation.py
+++ leaf-Segmentation.py
@@ -91,3 +91,28 @@ test_generator  = image_generator(test_files, batch_size = batch_size)
 
 x, y= next(train_generator)
 
+def mean_iou(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
+    inter = tf.math.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
+    union = tf.math.count_nonzero(tf.add(yt0, yp0))
+    iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
+    return iou
+  
+def mean_iou(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
+    inter = tf.math.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
+    union = tf.math.count_nonzero(tf.add(yt0, yp0))
+    iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
+    return iou
+
+"""# Training"""
+
+train_steps = len(train_files) //batch_size
+test_steps = len(test_files) //batch_size
+#model.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = [mean_iou])
+model.fit_generator(train_generator, 
+                    epochs = 100, steps_per_epoch = train_steps,validation_data = test_generator, validation_steps = 10,verbose = 2)
+
+model.save_weights('/content/gdrive/My Drive/training/weights.h5')
